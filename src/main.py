492e952b1690261338fd270be9fba3a7831e2c94
Theo <theo.guegan@etu.utc.fr>
2024-03-26 14:45:38 +0100

    re-orga

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
-from src.login import credentials
+from login import credentials
 from src.word_count import word_count
 from src.tag_count import tag_count
 
