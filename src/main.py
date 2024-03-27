11f66265e844bb6bdeebcd7027e5754e0755c95e
Theo <theo.guegan@etu.utc.fr>
2024-03-27 09:40:26 +0100

    correction des problemes

--- src/main.py
+++ src/main.py
@@ -24,7 +24,7 @@ with open("src/blacklist.txt", "r") as file:
     blacklist = {line.strip() for line in file}
 
 options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
+#options.add_argument("--headless=new")
 driver = Chrome(options=options)
 
 
@@ -36,11 +36,29 @@ def get_page_title(parser):
     title_tag = parser.find("title")
     return title_tag.text if title_tag else "Titre non trouvé"
 
-def get_extension():
-    driver.current_url
-    driver.implicitly_wait(2)
-    extension = os.path.splitext(driver.current_url)[1]
+def get_extension(url):
+    if url[-1] == '/':
+        return "htlm"
+    
+    # Trouver la dernière occurrence du caractère '.' dans l'URL
+    dot_index = url.rfind('.')
+    
+    # Si aucun '.' n'est trouvé dans l'URL, renvoyer une chaîne vide
+    if dot_index == -1:
+        return ""
+    
+    # Extraire l'extension du fichier en utilisant le reste de l'URL après le dernier '.'
+    extension = url[dot_index + 1:]
+    
+    # Si l'extension contient des paramètres de requête, les supprimer
+    if '?' in extension:
+        extension = extension[:extension.index('?')]
+
+    if "jsf" in extension:
+        return "jsf"
+    
     return extension
+
 
 
 # ------------------------ Fonctions de scrapping ------------------------
@@ -55,43 +73,61 @@ def get_links(parser, page_url):
     # on recupere le lien absolu
     for link in links:
         href = link.get("href")
-        if href and (href[0] != "#") and ("null" not in href):
+        if href and (href[0] != "#") and not href.startswith("mailto:") and not href.startswith("tel:"):
             # si le lien est relatif
             if ("http" or "https") not in href:
                 full_link = urljoin(page_url, href)
-                result.append(full_link)
+                #distinction site interne / externe à l'ent
+                if "https://webapplis.utc.fr" not in full_link:
+                    result.append((full_link,0))
+                else:
+                    result.append((full_link,1))
             # sinon
             else:
-                result.append(href)
+                #distinction site interne / externe à l'ent
+                if "https://webapplis.utc.fr" not in href:
+                    result.append((href,0))
+                else:
+                    result.append((href,1))
     return result
 
 
 # Fonction pour scraper les pages en profondeur
 def scrape_page(url, depth=0, source=None):
-    # Vérifier si la page a déjà été visitée
+    # Si la page à déjà été visitée, on ne la traite pas
     if url in visited_pages:
         return
     
+    # provisoir pur les tests
     if depth > 2 :
+        print("depth > 2")
         return
+    
+    # Ajouter la page à la liste des pages visitées
+    visited_pages.add(url)
+    
+    #on récupère les infos de la page pour ajouter un noeud au graphe
+    extension = get_extension(url)
+    print(extension)
+    G.add_node(url, extension=extension, depth=depth)
 
+    # Scrapping de la page ----------------------------------------------
     # On ne traite que les pages de l'ENT
     if "https://webapplis.utc.fr" not in url:
         return
-    
-    extension = get_extension()
+    #On ne scrap pas les pdf, docx etc..
     if extension in blacklist:
         return
 
-    # Ajouter la page à la liste des pages visitées
-    visited_pages.add(url)
-
+    # Si la page n'a pas de titre, on ne la traite pas
+    if not driver.title or driver.title == "404 Not Found":
+        return
+    
     driver.get(url)
     parser = BeautifulSoup(driver.page_source, "html.parser")
-
-    # Appel des différentes fonctions de traitement
+    
     title = get_page_title(parser)
-    links = get_links(parser, url)
+    links = get_links(parser, url) # retourne une liste de tuple (url, internal) ave internal = 1 si le lien est interne à l'ent
     words = word_count(parser)
     tags = tag_count(parser)
 
@@ -100,12 +136,11 @@ def scrape_page(url, depth=0, source=None):
     if source:
         G.add_edge(source, url)
 
-
-    print(f"{'  ' * depth} - {title} ({len(links)} liens, {words[0]} mots, {tags} balises)")
+    #print(f"{'  ' * depth} - {title} ({len(links)} liens, {words[0]} mots, {tags} balises)")
 
     # Appel récursif pour les pages en dessous
     for link in links:
-        scrape_page(link, depth + 1, url)
+        scrape_page(link[0], depth + 1, url)
 
 
 # ------------------------ Connexion + Scrapping ------------------------
