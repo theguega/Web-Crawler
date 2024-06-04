0f85731e3b6efa963e46c92feaa8e33197af7785
Theo <theo.guegan@etu.utc.fr>
2024-06-04 14:29:03 +0200

    fix export

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ def main():
     scraper = scp.Scraper(username=credentials["username"], password=credentials["password"])
     graph = scraper.get_data()
 
-    data_parser = jsp.JSONParser()
-    data_parser.parse('../data_json/merged_data.json')
-    activity_graph = data_parser.get_data()
+    #data_parser = jsp.JSONParser()
+    #data_parser.parse('../data_json/merged_data.json')
+    #activity_graph = data_parser.get_data()
 
 
 if __name__ == "__main__":
